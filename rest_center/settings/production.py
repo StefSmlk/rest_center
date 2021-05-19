db8bf6bf92e95b4b83769084c5cbc04d9cb8fa64
StefsmlK <gsstteeff7777@gmail.com>
2021-05-19 17:18:49 +0300

    добавил фикстуры

--- rest_center/settings/production.py
+++ rest_center/settings/production.py
@@ -31,7 +31,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rest-center.herokuapp.com']
 
 # Application definition
 
